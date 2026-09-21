c5399bf2f385bbbda8c767fad0375eef16ef9c97
afiffaizun <mafifdev31@gmail..com>
2026-09-21 17:27:04 +0700

    feat: add Go app with KEDA autoscaling

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,10 +16,22 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "OK")
 }
 
+func cpuHandler(w http.ResponseWriter, r *http.Request) {
+	end := time.Now().Add(5 * time.Second)
+
+	for time.Now().Before(end) {
+		for i := 0; i < 1000000; i++ {
+			_ = i * i
+		}
+	}
+
+	fmt.Fprintln(w, "CPU load generated")
+}
+
 func main() {
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/health", healthHandler)
-
+	http.HandleFunc("/cpu", cpuHandler)
 	port := ":8080"
 
 	log.Printf("Server running on port %s", port)
